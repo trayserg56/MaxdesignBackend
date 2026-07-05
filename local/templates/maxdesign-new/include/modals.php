2d03eed67cf15414598cb604db15a63de4912faf
Vladimir <jaspnstadzam@gmail.com>
2026-07-05 16:01:44 +0300

    Добавил новые модалки сабмита форм

--- local/templates/maxdesign-new/include/modals.php
+++ local/templates/maxdesign-new/include/modals.php
@@ -1,5 +1,10 @@
 <?php
 
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+
+$contacts = \Entity\Contacts::getInstance();
 ?>
 <div class="modal-wrapper modal-wrapper--example" id="modal-example" data-modal>
     <div class="modal">
@@ -15,3 +20,41 @@
         </div>
     </div>
 </div>
+
+<div class="modal-wrapper modal-wrapper--example" id="form-submit-success" data-modal>
+    <div class="modal">
+        <div class="modal__wrap">
+            <div class="modal__header">Ваша заявка успешно отправлена</div>
+            <div class="modal__body">
+                <div class="modal__content"><p class="modal__text">
+                        Менеджер свяжется с вами в ближайшее время
+                    </p>
+                </div>
+            </div>
+            <div class="modal__footer">
+                <button class="button" onclick="Modal.close('form-submit-success');">Хорошо</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal-wrapper modal-wrapper--example" id="form-submit-error" data-modal>
+    <div class="modal">
+        <div class="modal__wrap">
+            <div class="modal__header">Произошла ошибка при отправке формы</div>
+            <div class="modal__body">
+                <div class="modal__content">
+                    <p class="modal__text" data-error-text></p>
+                    <p class="modal__text">
+                        Попробуйте заполнить форму ещё раз или позвоните по телефону
+                        <a href="tel:<?= $contacts->getFormatedPhone('phone') ?>"><?= $contacts->get('phone') ?></a>
+                    </p>
+                </div>
+            </div>
+            <div class="modal__footer">
+                <button class="button" onclick="Modal.close('form-submit-error');">Хорошо</button>
+            </div>
+        </div>
+    </div>
+</div>
+
