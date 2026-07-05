852fe7ea45bfcde07e15ad32b297d3931901e793
Vladimir <jaspnstadzam@gmail.com>
2026-07-05 16:01:34 +0300

    Сделал ajax обработку форм

--- local/templates/maxdesign-new/custom.js
+++ local/templates/maxdesign-new/custom.js
@@ -2,6 +2,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     initPaginationButtons();
     initFilterButtons();
+    initForms();
 
     function replaceWith(response, selectorAttribute = 'data-replace') {
         document.querySelectorAll(`[${selectorAttribute}]`).forEach(element => {
@@ -60,6 +61,42 @@ document.addEventListener('DOMContentLoaded', () => {
                         replaceWith(r, 'data-filter-container');
                         button.disabled = false;
                         window.history.pushState({}, '', url);
+                    }
+                });
+            });
+        });
+    }
+
+    function initForms() {
+        document.querySelectorAll('form')?.forEach(form => {
+            form.addEventListener('submit', function (event) {
+                event.preventDefault();
+
+                let data = new FormData(form);
+                data.append('formCode', form.id);
+                data.append('sessid', BX.bitrix_sessid());
+
+                BX.ajax({
+                    url: form.action,
+                    method: 'POST',
+                    data: Object.fromEntries(data),
+                    dataType: 'json',
+                    onsuccess: function (r) {
+                       if (r?.success) {
+                           Modal.open('form-submit-success');
+                       } else {
+                           if (r && r.errors) {
+                               document.querySelector('[data-error-text]').innerHTML = '';
+                               r.errors.forEach(error => {
+                                   document.querySelector('[data-error-text]').innerHTML += error.message + '<br>';
+                               });
+                           }
+
+                           Modal.open('form-submit-error');
+                       }
+                    },
+                    onfailure: function() {
+                        Modal.open('form-submit-error');
                     }
                 });
             });
