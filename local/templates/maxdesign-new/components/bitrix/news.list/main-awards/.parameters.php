d503103ea4aa91c56df9706acaf2065304f61438
Vladimir <jasonstadzam@gmail.com>
2026-07-22 20:23:52 +0300

    Фикс блока о нас и добавление в параметры компонента кол-ва наград

---
+++ local/templates/maxdesign-new/components/bitrix/news.list/main-awards/.parameters.php
@@ -0,0 +1,14 @@
+<?php
+
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+
+$arTemplateParameters = [
+    'AWARDS_AMOUNT' => [
+        'NAME' => 'Количество наград',
+        'PARENT' => 'BASE',
+        'TYPE' => 'STRING',
+        'DEFAULT' => '164 награды',
+    ],
+];
--- local/templates/maxdesign-new/components/bitrix/news.list/main-awards/template.php
+++ local/templates/maxdesign-new/components/bitrix/news.list/main-awards/template.php
@@ -5,6 +5,7 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
 }
 
 /** @var array $arResult */
+/** @var array $arParams */
 ?>
 <div class="about__col about__col--awards">
     <ul class="about__awards" aria-label="Награды и членство">
@@ -14,7 +15,7 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
                                           alt="<?= $item['NAME'] ?>" width="97" height="60" loading="lazy" decoding="async">
                 <p class="about__award-text"><?= $item['NAME'] ?></p></li>
         <?php } ?>
-        <li class="about__award about__award--summary"><p class="about__award-highlight">164 награды</p>
+        <li class="about__award about__award--summary"><p class="about__award-highlight"><?= $arParams['AWARDS_AMOUNT'] ?></p>
         </li>
     </ul>
 </div>
